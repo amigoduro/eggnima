0600b7a48d962ab4a149904fb57376e28dcde4d1
Cadet Adarsh <pal.adarsh10205@gmail.com>
2025-11-07 23:05:17 +0530

    Add basic addition program in C

---
+++ addition.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b,c,d,e,f;
+clrscr();
+printf("\nEnter 1st Number ");
+scanf("\n%d",&a);
+printf("\nEnter 2nd Number ");
+scanf("\n%d",&b);
+c=a+b;
+//a+b=c;
+printf("\n The Sum of two number's is %d ",c);
+//printf("\nThe No. is%d",a);
+getch();
+}
