23bfac47ff043b0a7b8e65ea72161fe8029bd4e6
Cadet Adarsh <pal.adarsh10205@gmail.com>
2026-08-11 21:53:22 +0530

    Update addition.c

--- addition.c
+++ addition.c
@@ -12,7 +12,7 @@ printf("\nEnter 2nd Number ");
 scanf("\n%d",&b);
 c=a+b;                                  // value assignation              
 //a+b=c;   
-printf("\n The Sum of two number's is %d ",c);
+printf("\n The Sum of two number's is %d ",c);   
 //printf("\nThe No. is%d",a);    
 getch();
 }
